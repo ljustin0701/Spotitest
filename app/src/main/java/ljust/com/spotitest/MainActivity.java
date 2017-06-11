574c660c94ad62ca723b7c6cf6e97ced09d87095
Justin Li <ljustin0701@gmail.com>
2017-06-11 17:43:33 -0400

    Added fragment that displays a blank RecyclerView

--- app/src/main/java/ljust/com/spotitest/MainActivity.java
+++ app/src/main/java/ljust/com/spotitest/MainActivity.java
@@ -1,13 +1,25 @@
 package ljust.com.spotitest;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ljust.com.spotitest.fragments.SpotFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFragmentManager = getSupportFragmentManager();
+    }
+
+    @Override
+    protected void onStart() {
+        mFragmentManager.beginTransaction().add(R.id.fragment_container, SpotFragment.create(), "spot").commit();
+        super.onStart();
     }
 }
